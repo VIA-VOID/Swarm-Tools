9823e4c282ed9650a8d3d2b5f6d77bdace937d32
jeondoh <jundo1302@gmail.com>
2025-05-17 02:29:37 +0900

    fix: namespace 제거

--- PacketProtobuf/Program.cs
+++ PacketProtobuf/Program.cs
@@ -8,16 +8,13 @@
   패킷 자동화 코드 작성
 --------------------------------------------------------*/
 
-namespace PacketProtobuf
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // 서버 자동화 코드 실행
-            ServerAutoGenerate.GenerateServer(args);
-            // 클라이언트(unity) 자동화 코드 실행
-            ClientAutoGenerate.GenerateClient(args);
-        }
+        // 서버 자동화 코드 실행
+        ServerAutoGenerate.GenerateServer(args);
+        // 클라이언트(unity) 자동화 코드 실행
+        ClientAutoGenerate.GenerateClient(args);
     }
 }
