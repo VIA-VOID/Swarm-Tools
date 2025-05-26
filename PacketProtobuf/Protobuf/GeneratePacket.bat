14721a293c8639e79693d82954e4741a8b220369
jeondoh <jundo1302@gmail.com>
2025-05-27 00:03:25 +0900

    feat: dummyClient 자동화 추가, C# out 제거

--- PacketProtobuf/Protobuf/GeneratePacket.bat
+++ PacketProtobuf/Protobuf/GeneratePacket.bat
@@ -7,7 +7,6 @@ set PROJ_PATH=%CD%\..
 set CS_PROJ=%PROJ_PATH%\PacketProtobuf.csproj
 set SERVER_PATH=..\..\..\SwarmServer\GameServer
 set DUMMY_CLIENT_PATH=..\..\..\SwarmServer\DummyClient
-set UNITY_CLIENT_PATH=..\..\..\SwarmClient\Client\Assets\Scripts
 
 echo [INFO] Current Directory: %CD%
 echo [INFO] Project Directory: %CS_PROJ%
@@ -21,10 +20,10 @@ IF ERRORLEVEL 1 (
     PAUSE
 )
 
-:: .proto -> C++ & C# 파일 생성
-protoc -I=./ --cpp_out=./ --csharp_out=./ ./Protocol.proto
-protoc -I=./ --cpp_out=./ --csharp_out=./ ./Struct.proto
-protoc -I=./ --cpp_out=./ --csharp_out=./ ./Enum.proto
+:: .proto -> C++ 파일 생성
+protoc -I=./ --cpp_out=./ ./Protocol.proto
+protoc -I=./ --cpp_out=./ ./Struct.proto
+protoc -I=./ --cpp_out=./ ./Enum.proto
 
 IF ERRORLEVEL 1 (
     echo [ERROR] protoc failed!
@@ -39,7 +38,8 @@ XCOPY /Y Struct.pb.h "%SERVER_PATH%\Protocol"
 XCOPY /Y Struct.pb.cc "%SERVER_PATH%\Protocol"
 XCOPY /Y Protocol.pb.h "%SERVER_PATH%\Protocol"
 XCOPY /Y Protocol.pb.cc "%SERVER_PATH%\Protocol"
-XCOPY /Y ServerPacketHandler.h "%SERVER_PATH%\Packet"
+XCOPY /Y PacketHandler.cpp "%SERVER_PATH%\Packet"
+XCOPY /Y PacketId.h "%SERVER_PATH%\Packet"
 
 echo [INFO] Copying generated files to DummyClient...
 
@@ -49,22 +49,15 @@ XCOPY /Y Struct.pb.h "%DUMMY_CLIENT_PATH%\Protocol"
 XCOPY /Y Struct.pb.cc "%DUMMY_CLIENT_PATH%\Protocol"
 XCOPY /Y Protocol.pb.h "%DUMMY_CLIENT_PATH%\Protocol"
 XCOPY /Y Protocol.pb.cc "%DUMMY_CLIENT_PATH%\Protocol"
-XCOPY /Y ClientPacketHandler.h "%DUMMY_CLIENT_PATH%\Packet"
-
-echo [INFO] Copying generated files to SwarmClient...
-XCOPY /Y Enum.cs "%UNITY_CLIENT_PATH%\Protocol"
-XCOPY /Y Struct.cs "%UNITY_CLIENT_PATH%\Protocol"
-XCOPY /Y Protocol.cs "%UNITY_CLIENT_PATH%\Protocol"
-XCOPY /Y PacketHeader.cs "%UNITY_CLIENT_PATH%\Packet"
-XCOPY /Y PacketHandler.cs "%UNITY_CLIENT_PATH%\Packet"
-XCOPY /Y PacketSystem.cs "%UNITY_CLIENT_PATH%\Packet"
+XCOPY /Y PacketHandler.cpp "%DUMMY_CLIENT_PATH%\Packet"
+XCOPY /Y PacketId.h "%DUMMY_CLIENT_PATH%\Packet"
 
 :: 생성된 파일 정리
 echo [INFO] Cleaning up temporary files...
 DEL /Q /F *.pb.h
 DEL /Q /F *.pb.cc
 DEL /Q /F *.h
-DEL /Q /F *.cs
+DEL /Q /F *.cpp
 
 :: 완료
 echo [SUCCESS] All generation completed.
